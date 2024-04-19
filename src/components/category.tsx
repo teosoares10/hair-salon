b099869ac0e3dcb82100465300b8db8d3357ccf5
Teodoro Pedro <teosoares10@gmail.com>
2024-04-19 15:35:08 +0100

    feat: create category component

---
+++ src/components/category.tsx
@@ -0,0 +1,10 @@
+import { CategoryItem } from "./category-item"
+
+export const Category = () => {
+    return (
+        <section className="flex gap-8 justify-center items-center my-8">
+            <CategoryItem img="/img/banner.png" alt="" title="perucas" />
+            <CategoryItem img="/img/hair.png" alt="" title="cabelos" />
+        </section>
+    )
+}
