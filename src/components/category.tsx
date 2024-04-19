56cf90d5eee5bf16bd05523f4bbb9111fad1142c
Teodoro Pedro <teosoares10@gmail.com>
2024-04-19 16:09:04 +0100

    styles(category): add responsive style

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -2,7 +2,7 @@ import { CategoryItem } from "./category-item"
 
 export const Category = () => {
     return (
-        <section className="flex gap-8 justify-center items-center my-8">
+        <section className="flex gap-8 flex-wrap justify-center items-center my-8">
             <CategoryItem img="/img/banner.png" alt="" title="perucas" />
             <CategoryItem img="/img/hair.png" alt="" title="cabelos" />
         </section>
