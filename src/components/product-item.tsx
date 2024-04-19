1c6bc46bb13a3254e73674497801650b3348f7be
Teodoro Pedro <teosoares10@gmail.com>
2024-04-19 15:36:18 +0100

    feat: create product item component

---
+++ src/components/product-item.tsx
@@ -0,0 +1,14 @@
+import { ProductItemProps } from "@/types/product-item.types"
+import Image from "next/image"
+import Link from "next/link"
+import { Typography } from "./ui/typography"
+
+export const ProductItem = ({ title, img, alt, price, href }: ProductItemProps) => {
+    return (
+        <Link href={href} className="inline-block w-[14rem] group">
+            <Image src={img} alt={`${alt}`} width={155} height={255} className="w-full h-full" />
+            <Typography as="h4" className="my-4 font-bold group-hover:underline">{title}</Typography>
+            <Typography as="span">{price} Akz</Typography> 
+        </Link>
+    )
+}
