205b6df1d0ee0fd0604122c9b2143a4158d2b7f5
Teodoro Pedro <teosoares10@gmail.com>
2024-04-19 15:23:14 +0100

    feat(navbar): add contact and menu links sections

--- src/components/layout/navbar.tsx
+++ src/components/layout/navbar.tsx
@@ -1,9 +1,35 @@
 import Link from "next/link"
+import { Typography } from "../ui/typography"
+import { PhoneCall, MapPin } from 'lucide-react'
+import React from "react"
+import Image from "next/image"
 
 export const Navbar = () => {
     return (
         <nav>
-            
+            <section className="flex justify-between items-center py-2 px-4">
+                <div className="flex items-center gap-2">
+                    <MapPin />
+                    <Typography as="span">
+                        Kero Benguela, Av Gen. João de Almeida, Benguela, Angola
+                    </Typography>
+                </div>
+                <div className="flex gap-4 items-center">
+                    <PhoneCall />
+                    <Typography as="span" className="text-lg tracking-widest">957704613</Typography>
+                </div>
+            </section>
+
+            <section className="flex justify-between items-center bg-gray-600 p-4 text-white">
+                <Link href="/">
+                    <Image src="/img/logo.png" alt="" className="" width={50} height={50} />
+                </Link>
+                <div className="space-x-8 text-md">
+                    <Link href="/">Inicio</Link>
+                    <Link href="/about">Sobre</Link>
+                    <Link href="/contact">Contato</Link>
+                </div>
+            </section>
         </nav>
     )
 }
