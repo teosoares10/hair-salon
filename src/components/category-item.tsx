562f8b0a01ddb4fefb86067bc2fbb08903d6927c
Teodoro Pedro <teosoares10@gmail.com>
2024-04-19 19:45:57 +0100

    feat(category-item): add description prop
     - set new value for image width, height attributes and position absolute
     - create description wrapper which has title, description and button

--- src/components/category-item.tsx
+++ src/components/category-item.tsx
@@ -3,25 +3,28 @@ import Image from "next/image"
 import { Typography } from "./ui/typography"
 import { Button } from "./ui/button"
 
-export const CategoryItem = ({img, alt, title}: CategoryItemProps) => {
+export const CategoryItem = ({img, alt, title, description}: CategoryItemProps) => {
     return (
-        <div className="w-[31rem] h-[20rem] relative group">
+        <div className="w-[38rem] h-[20rem] relative bg-gray-600">
             <Image 
                 src={img} 
                 alt={`${alt}`} 
-                width={500} 
-                height={185} 
-                className=" h-full bg-gray-600" 
+                width={280} 
+                height={520} 
+                className="h-full absolute -right-10 bottom-0" 
             />
-
-            <Button
-                as="link" 
-                href="/shop" 
-                className={`hidden px-[4rem] absolute lg:left-[30%] left-[25%] top-[40%] origin-center capitalize 
-                    text-white text-xl group-hover:inline-block`}
-            >
-                {title}
-            </Button>
+            
+            <div className="pl-8 pt-8">
+                <Typography as="h3" className="w-[25rem] text-white text-2xl font-bold">{title}</Typography>
+                <Typography className="w-[24rem] py-4">{description}</Typography>
+                <Button
+                    as="link" 
+                    href="/shop" 
+                    className="absolute first-letter:uppercase lowercase text-white text-lg px-8"
+                >
+                    ir a loja
+                </Button>
+            </div>
         </div>
     )
 }
