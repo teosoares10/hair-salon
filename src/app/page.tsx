c6be5b14407e2f03dc0b041da97089c21df5902c
Teodoro Pedro <teosoares10@gmail.com>
2024-04-19 16:07:36 +0100

    styles(home): add responsive style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,14 +7,14 @@ import Image from "next/image";
 export default function Home() {
   return (
     <section>
-      <section className=" flex gap-4 justify-center items-center bg-zinc-800">
-        <div className="text-left">
-          <Typography className="w-[26rem] text-6xl font-light text-white uppercase leading-tight">
+      <section className="flex flex-wrap gap-4 justify-center items-center bg-zinc-800">
+        <div className="lg:text-left text-center pt-8 lg:pt-auto">
+          <Typography className="lg:w-[26rem] lg:text-6xl text-5xl long:font-light font-bold text-white uppercase leading-tight">
             linda. glamorosa. confiante.
           </Typography>
           <Button as="link" href="" className="inline-block mt-8 text-black">compre agora</Button>
         </div>
-        <Image src="/img/image.png" alt="" width={650} height={850} />
+        <Image src="/img/image.png" alt="" width={650} height={850} className="mt-4 md:mt-0" />
       </section>
 
       <Category />
