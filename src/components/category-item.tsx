3b7de37fb6d71b9f348052d8827524380059dc73
Teodoro Pedro <teosoares10@gmail.com>
2024-04-19 20:14:56 +0100

    styles(category-item): add responsive

--- src/components/category-item.tsx
+++ src/components/category-item.tsx
@@ -5,18 +5,18 @@ import { Button } from "./ui/button"
 
 export const CategoryItem = ({img, alt, title, description}: CategoryItemProps) => {
     return (
-        <div className="w-[38rem] h-[20rem] relative bg-gray-600">
+        <div className="lg:w-[38rem] w-full h-[20rem] relative bg-gray-600 mx-4 lg:mx-0 rounded-lg">
             <Image 
                 src={img} 
                 alt={`${alt}`} 
                 width={280} 
                 height={520} 
-                className="h-full absolute -right-10 bottom-0" 
+                className="h-full absolute -right-10 bottom-0 opacity-20" 
             />
             
-            <div className="pl-8 pt-8">
-                <Typography as="h3" className="w-[25rem] text-white text-2xl font-bold">{title}</Typography>
-                <Typography className="w-[24rem] py-4">{description}</Typography>
+            <div className=" pl-4 lg:pl-8 pt-4 lg:pt-8">
+                <Typography as="h3" className="lg:w-[25rem] text-white text-2xl font-bold">{title}</Typography>
+                <Typography className="lg:w-[24rem] text-gray-200 py-4">{description}</Typography>
                 <Button
                     as="link" 
                     href="/shop" 
