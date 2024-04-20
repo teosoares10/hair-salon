dedef42e01d2402da915d71795921f21b84c8c4c
Teodoro Pedro <teosoares10@gmail.com>
2024-04-20 23:12:55 +0100

    styles(category): set space top and bottom for small and large screens

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -2,7 +2,7 @@ import { CategoryItem } from "./category-item"
 
 export const Category = () => {
     return (
-        <section className="flex gap-8 flex-wrap justify-center items-center mt-8 mb-16">
+        <section className="flex gap-8 flex-wrap justify-center items-center mt-8 mb-16 space-y-4 lg:space-y-0">
             <CategoryItem 
                 img="/img/banner.png" 
                 alt="" 
