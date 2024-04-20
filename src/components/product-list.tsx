dcfea4d007a686ee86bc03fc0e261ec664dac64b
Teodoro Pedro <teosoares10@gmail.com>
2024-04-20 20:04:54 +0100

    refactor(product-list): remove price attribute from product item

--- src/components/product-list.tsx
+++ src/components/product-list.tsx
@@ -4,47 +4,38 @@ import { Typography } from "./ui/typography"
 const data = [
     {
       "nome": "Peruca Longa Lisa",
-      "preco": 120000.00,
       "imagem": "/img/outre-synthetic-hd-transparent-lace-front-wig-hudson.webp"
     },
     {
       "nome": "Peruca Curta Cacheada",
-      "preco": 80000.00,
       "imagem": "/img/sensationnel-dashly-synthetic-lace-front-wig-lace-unit.webp"
     },
     {
       "nome": "Peruca Bob",
-      "preco": 150000.00,
       "imagem": "/img/sensationnel-synthetic-kinky-edge-13x6-hd-lace-wig-kinky-blow-out-1.webp"
     },
     {
       "nome": "Peruca Dreads",
-      "preco": 100000.00,
       "imagem": "/img/outre-synthetic-hd-transparent-lace-front-wig-hudson.webp"
     },
     {
       "nome": "Peruca Rabo de Cavalo",
-      "preco": 90.00,
       "imagem": "/img/outre-synthetic-hd-transparent-lace-front-wig-hudson.webp"
     },
     {
       "nome": "Peruca Ombré",
-      "preco": 180.00,
       "imagem": "/img/outre-synthetic-hd-transparent-lace-front-wig-hudson.webp"
     },
     {
       "nome": "Peruca Cosplay",
-      "preco": 200.00,
       "imagem": "/img/outre-synthetic-hd-transparent-lace-front-wig-hudson.webp"
     },
     {
       "nome": "Peruca Masculina",
-      "preco": 130.00,
       "imagem": "/img/outre-synthetic-hd-transparent-lace-front-wig-hudson.webp"
     },
     // {
     //   "nome": "Peruca Infantil",
-    //   "preco": 60.00,
     //   "imagem": "/img/outre-synthetic-hd-transparent-lace-front-wig-hudson.webp"
     // }
   ]
@@ -63,7 +54,6 @@ export const ProductList = () => {
                     <ProductItem
                         key={i} 
                         img={wig.imagem}
-                        price={wig.preco}
                         title={wig.nome}
                         href=""
                     />
