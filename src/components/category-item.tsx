340db423ef616d268431d2733b659cc07b65473f
Teodoro Pedro <teosoares10@gmail.com>
2024-04-20 20:33:49 +0100

    styles(category-item): set new border radius

--- src/components/category-item.tsx
+++ src/components/category-item.tsx
@@ -4,7 +4,7 @@ import { Typography } from "./ui/typography"
 
 export const CategoryItem = ({ img, alt, title }: CategoryItemProps) => {
     return (
-        <div className="lg:w-[20rem] w-full h-[26rem] bg-gray-600 mx-4 lg:mx-0 rounded-lg">
+        <div className="lg:w-[20rem] w-full h-[26rem] bg-gray-600 mx-4 lg:mx-0 rounded-3xl">
             <Image 
                 src={img} 
                 alt={`${alt}`} 
