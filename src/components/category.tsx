b5ec50dd99bd32558a49cd682b6b913dab0d327a
Teodoro Pedro <teosoares10@gmail.com>
2024-04-20 19:35:15 +0100

    refactor(category): remove description attribute from category item

--- src/components/category.tsx
+++ src/components/category.tsx
@@ -7,14 +7,12 @@ export const Category = () => {
                 img="/img/banner.png" 
                 alt="" 
                 title="Transforme seu visual com as nossas perucas!"
-                description="Descubra uma variedade incrível de perucas de alta qualidade para todos os estilos e ocasiões. Encontre a peruca perfeita para você e surpreenda a todos com um novo visual!" 
             />
 
             <CategoryItem 
                 img="/img/hair.png" 
                 alt="" 
                 title="Alongue e volumize seus cabelos com os nossos cabelos!"
-                description="Adicione comprimento, volume e estilo aos seus cabelos com as nossas hair extensions de alta qualidade. Diversas opções disponíveis para combinar com seu cabelo natural." 
             />
         </section>
     )
