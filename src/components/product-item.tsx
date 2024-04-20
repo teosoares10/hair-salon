23b6d7c29948470c7297a1f2421e2b20b565ea06
Teodoro Pedro <teosoares10@gmail.com>
2024-04-20 20:31:42 +0100

    styles(product-item): set border radius to image

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -6,7 +6,7 @@ import { Typography } from "./ui/typography"
 export const ProductItem = ({ title, img, alt, href }: ProductItemProps) => {
     return (
         <Link href={href} className="inline-block w-[14rem] group">
-            <Image src={img} alt={`${alt}`} width={155} height={255} className="w-full h-full" />
+            <Image src={img} alt={`${alt}`} width={155} height={255} className="w-full h-full rounded-3xl" />
             <Typography as="h4" className="my-4 font-bold group-hover:underline">{title}</Typography>
         </Link>
     )
