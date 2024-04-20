01ca487d5846c5b2818168f6625877997b6df03a
Teodoro Pedro <teosoares10@gmail.com>
2024-04-20 19:47:45 +0100

    refactor(category-item): remove wrapper from typography

--- src/components/category-item.tsx
+++ src/components/category-item.tsx
@@ -12,10 +12,7 @@ export const CategoryItem = ({ img, alt, title }: CategoryItemProps) => {
                 height={520} 
                 className="h-full w-full" 
             />
-            
-            <div className=" pl-4 lg:pl-8 pt-4 lg:pt-8">
-                <Typography as="h3" className="lg:w-[25rem] text-white text-2xl font-bold">{title}</Typography>
-            </div>
+            <Typography as="h3" className="lg:w-[25rem] text-white text-2xl font-bold">{title}</Typography>
         </div>
     )
 }
