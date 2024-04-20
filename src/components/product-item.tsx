e6cbcf4b9930949680374293db265a2eaa725b03
Teodoro Pedro <teosoares10@gmail.com>
2024-04-20 20:02:53 +0100

    refactor(product-item): remove price Typography

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -3,12 +3,11 @@ import Image from "next/image"
 import Link from "next/link"
 import { Typography } from "./ui/typography"
 
-export const ProductItem = ({ title, img, alt, price, href }: ProductItemProps) => {
+export const ProductItem = ({ title, img, alt, href }: ProductItemProps) => {
     return (
         <Link href={href} className="inline-block w-[14rem] group">
             <Image src={img} alt={`${alt}`} width={155} height={255} className="w-full h-full" />
             <Typography as="h4" className="my-4 font-bold group-hover:underline">{title}</Typography>
-            <Typography as="span">{price} Akz</Typography> 
         </Link>
     )
 }
