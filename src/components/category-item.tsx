05588ffc75fa64d71038184809f0696a99a442d2
Teodoro Pedro <teosoares10@gmail.com>
2024-04-20 19:51:47 +0100

    style(category-item): set align text

--- src/components/category-item.tsx
+++ src/components/category-item.tsx
@@ -12,7 +12,7 @@ export const CategoryItem = ({ img, alt, title }: CategoryItemProps) => {
                 height={520} 
                 className="h-full w-full" 
             />
-            <Typography as="h3" className=" text-gray-600 text-2xl font-bold">{title}</Typography>
+            <Typography as="h3" className="text-center text-gray-600 text-2xl font-bold pt-2">{title}</Typography>
         </div>
     )
 }
