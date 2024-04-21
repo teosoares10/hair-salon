28299c5085f96534fcb7a2c3cc24c0d4b8b4fee7
Teodoro Pedro <teosoares10@gmail.com>
2024-04-21 22:32:24 +0100

    refactor(product-item): remove title component

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -3,11 +3,10 @@ import Image from "next/image"
 import Link from "next/link"
 import { Typography } from "./ui/typography"
 
-export const ProductItem = ({ title, img, alt, href }: ProductItemProps) => {
+export const ProductItem = ({ img, alt, href }: ProductItemProps) => {
     return (
         <Link href={href} className="inline-block w-[24rem]">
             <Image src={img} alt={`${alt}`} width={555} height={255} className="size-[16rem] rounded-3xl" />
-            <Typography as="h4" className="my-4 font-bold">{title}</Typography>
         </Link>
     )
 }
