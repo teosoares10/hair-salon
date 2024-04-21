386c25981d9a035db171304262c31a7ae3ec1815
Teodoro Pedro <teosoares10@gmail.com>
2024-04-21 22:58:20 +0100

    styles(product-item): set new width

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -1,11 +1,10 @@
 import { ProductItemProps } from "@/types/product-item.types"
 import Image from "next/image"
 import Link from "next/link"
-import { Typography } from "./ui/typography"
 
 export const ProductItem = ({ img, alt, href }: ProductItemProps) => {
     return (
-        <Link href={href} className="inline-block w-[24rem]">
+        <Link href={href} className="inline-block w-[16rem]">
             <Image src={img} alt={`${alt}`} width={555} height={255} className="size-[16rem] rounded-3xl" />
         </Link>
     )
