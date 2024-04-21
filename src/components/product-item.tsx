bf660531c4b4a8a8da84850a0d4eef57f94a1eb7
Teodoro Pedro <teosoares10@gmail.com>
2024-04-21 20:20:05 +0100

    styles(product-item): remove group from product item link wrapper and group hover from typography

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -5,9 +5,9 @@ import { Typography } from "./ui/typography"
 
 export const ProductItem = ({ title, img, alt, href }: ProductItemProps) => {
     return (
-        <Link href={href} className="inline-block w-[24rem] group">
+        <Link href={href} className="inline-block w-[24rem]">
             <Image src={img} alt={`${alt}`} width={555} height={255} className="size-[16rem] rounded-3xl" />
-            <Typography as="h4" className="my-4 font-bold group-hover:underline">{title}</Typography>
+            <Typography as="h4" className="my-4 font-bold">{title}</Typography>
         </Link>
     )
 }
