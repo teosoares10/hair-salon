f76d010d16fda3900799784118b680d3ab13e9cb
Teodoro Pedro <teosoares10@gmail.com>
2024-04-22 00:05:46 +0100

    styles(product-item): set new width and height for image

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -5,7 +5,7 @@ import Link from "next/link"
 export const ProductItem = ({ img, alt, href }: ProductItemProps) => {
     return (
         <Link href={href} className="inline-block w-[16rem]">
-            <Image src={img} alt={`${alt}`} width={555} height={255} className="size-[16rem]" />
+            <Image src={img} alt={`${alt}`} width={555} height={255} className="w-[20rem] h-[24rem]" />
         </Link>
     )
 }
