2f4a671a2fefff1a2aa6001a8aedf6aae31a46c5
Teodoro Pedro <teosoares10@gmail.com>
2024-04-22 00:35:23 +0100

    styles(product-item): set new width for large screens

--- src/components/product-item.tsx
+++ src/components/product-item.tsx
@@ -4,7 +4,7 @@ import { Button } from "./ui/button"
 
 export const ProductItem = ({ img, alt, href }: ProductItemProps) => {
     return (
-        <section className="w-[16rem] relative group">
+        <section className="lg:w-[16.45rem] relative group">
             <Image src={img} alt={`${alt}`} width={555} height={255} className="w-[20rem] h-[23rem]" />
             <div className="hidden group-hover:flex justify-center items-center absolute top-0 left-0 w-full h-full">
                 <div className="flex justify-center items-center w-[14rem] h-[20rem] bg-zinc-900">
